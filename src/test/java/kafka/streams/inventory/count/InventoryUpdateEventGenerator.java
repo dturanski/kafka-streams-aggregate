f54de6e65ef444923067382b2a8c7b2f07cf5dee
David Turanski <dturanski@pivotal.io>
2019-10-03 09:11:36 -0400

    Yet more clean up

--- src/test/java/kafka/streams/inventory/count/InventoryUpdateEventGenerator.java
+++ src/test/java/kafka/streams/inventory/count/InventoryUpdateEventGenerator.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package kafka.streams.inventory.count;
 
 import java.util.Collections;
@@ -16,7 +31,6 @@ import static kafka.streams.inventory.count.InventoryUpdateEvent.Action.REP;
 
 /**
  * Test fixture to generate {@link InventoryUpdateEvent}s to a topic and maintain the state of the expected aggregated inventory counts.
- *
  */
 class InventoryUpdateEventGenerator {
 
@@ -70,7 +84,7 @@ class InventoryUpdateEventGenerator {
 
 
                 logger.debug("Sending inventoryUpdateEvent: key {} delta {} action {}",
-                        inventoryUpdateEvent.getKey().getProductCode() ,inventoryUpdateEvent.getDelta(),inventoryUpdateEvent.getAction());
+                        inventoryUpdateEvent.getKey().getProductCode(), inventoryUpdateEvent.getDelta(), inventoryUpdateEvent.getAction());
 
                 kafkaTemplate.sendDefault(inventoryUpdateEvent.getKey(), inventoryUpdateEvent);
 
